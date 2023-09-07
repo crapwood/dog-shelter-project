84a90e67035bbd267afcd203a965e5ab05555534
John James Benitez <jbenitez@proofpoint.com>
2023-09-07 22:09:17 +0300

    add new entry (temporary)

--- src/store/global-items.store.ts
+++ src/store/global-items.store.ts
@@ -1,5 +1,6 @@
+// @ts-nocheck
 import { create } from 'zustand';
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 import { tempData } from "@/constants/constants";
 
 
